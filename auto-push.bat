784dd31afe60f00225b9222d1249d7b0015be338
SilentGene <linheyu@outlook.com>
2018-09-11 15:44:31 +1000

    Auto Updated

--- auto-push.bat
+++ auto-push.bat
@@ -1,6 +1,9 @@
 @echo off
 @title bat auto commit and push code
 
+E:
+cd E:\Biology_data\0.Commands\2.MyPythonScript\rss-syn
+python rss-syn.py
 git add .
 git commit -m "Auto Updated"
 git push origin master
